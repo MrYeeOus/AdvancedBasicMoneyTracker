79c8cc09bf0b5a1484db24589fef1aa7d8ee0f5c
MrYeeOus <benwmyee@gmail.com>
2025-04-30 23:21:04 +1000

    Change package name + add apk

--- advanced_basic_money_tracker_2/android/app/src/main/kotlin/com/example/advanced_basic_money_tracker_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.advanced_basic_money_tracker_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ advanced_basic_money_tracker_2/android/app/src/main/kotlin/com/mryeeous/abmt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mryeeous.abmt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
